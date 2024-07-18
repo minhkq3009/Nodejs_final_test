6921c9ddb593c191297f7427135377d12247db35
minhkq3009 <55178535+minhkq3009@users.noreply.github.com>
2024-07-18 10:18:12 +0700

    Update product.routes.js

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,45 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../model/products.model');
+const productController = require('../controller/product.controller');
 
 // Lấy tất cả sản phẩm
-router.get('/', async (req, res) => {
-  try {
-    const products = await Product.find().sort({ ProductStoreCode: -1 });
-    res.render('index', { products });
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+router.get('/', productController.getAllProducts);
 
 // Thêm sản phẩm mới
-router.post('/', async (req, res) => {
-  const { ProductCode, ProductName, ProductDate, ProductOriginPrice, Quantity, ProductStoreCode } = req.body;
-  const product = new Product({
-    ProductCode,
-    ProductName,
-    ProductDate: new Date(ProductDate),
-    ProductOriginPrice,
-    Quantity,
-    ProductStoreCode
-  });
-
-  try {
-    await product.save();
-    res.redirect('/products');
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+router.post('/', productController.addProduct);
 
 // Xóa sản phẩm
-router.post('/delete/:id', async (req, res) => {
-  try {
-    await Product.findByIdAndDelete(req.params.id);
-    res.redirect('/products');
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+router.post('/delete/:id', productController.deleteProduct);
 
 module.exports = router;
